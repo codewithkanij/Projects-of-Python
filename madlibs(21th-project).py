92eaec4dd4bd0aed51908f52ba52e77b8d348d18
kanij-fatema <kanijsworna29@gmail.com>
2022-12-11 21:32:59 +0600

    Add requirement file

---
+++ madlibs(21th-project).py
@@ -0,0 +1,11 @@
+# string concatenation (how to put string together)
+# suppose we want to create a string that says "subscribe to _____"
+# youtuber ="Kylin Ying"
+# some string variable
+
+# a few ways to do this
+
+youtuber = input()
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
